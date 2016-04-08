4d573a701dd75670612233f01e01547ae4c8be61
essenlive <perchais.quentin@gmail.com>
2016-04-09 00:50:51 +0200

    minor changes

--- client/views/interface/interface.js
+++ client/views/interface/interface.js
@@ -13,41 +13,7 @@ Template.interface.events({
     }
 });
 
-Template.interface.onRendered(function(){
+Template.layout.onRendered(function(){
     refresh();
-    dragula(
-        [
-            document.querySelector('#catalog'),
-            document.querySelector('#spout1'),
-            document.querySelector('#spout2'),
-            document.querySelector('#spout3'),
-            document.querySelector('#spout4'),
-            document.querySelector('#spout5'),
-            document.querySelector('#spout6'),
-            document.querySelector('#bin'),
-        ],
-        {
-            copy: function (el, source) {
-                return $(source).hasClass('copy');
-            },
-            accepts: function (el, target) {
-                return !$(target).hasClass('copy');
-            }
-        }
-    ).on('over', function (el, container) {
-        if($(container).hasClass('bin')){
-            container.className += ' hover';
-        }
-    }).on('out', function (el, container) {
-        if($(container).hasClass('bin')){
-            container.className = container.className.replace('hover', '');
-        }
-    }).on('dragend', function (el) {
-        refresh();
-    }).on('drop', function (el, target, source, sibling) {
-        if($(target).hasClass('spout')){
-            $(target).empty();
-            $(target).prepend($(el));
-        }
-    })
+    dragAndDrop();
 });
